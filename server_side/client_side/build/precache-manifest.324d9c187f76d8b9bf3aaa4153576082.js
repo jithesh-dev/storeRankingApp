eac2b712bed7efa5c1f4a96e59ab8234c1e54d8e
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-09-27 00:15:16 +0100

    Added Build folder

---
+++ server_side/client_side/build/precache-manifest.324d9c187f76d8b9bf3aaa4153576082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4269415f95bcd6f59c684e9f96b55296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c354ae5b5d43df6d4363",
+    "url": "/static/css/main.8a53eb33.chunk.css"
+  },
+  {
+    "revision": "1fcc2298fd4ad8c76031",
+    "url": "/static/js/2.5bc98a68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bc98a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c354ae5b5d43df6d4363",
+    "url": "/static/js/main.ae0526a2.chunk.js"
+  },
+  {
+    "revision": "4fb2ab502f331cc8be6a",
+    "url": "/static/js/runtime-main.49996094.js"
+  }
+]);
