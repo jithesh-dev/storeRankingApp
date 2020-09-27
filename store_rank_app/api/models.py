8895aa1754ef8a2f1641d1cd38e115a2b75560ff
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-09-27 12:42:03 +0100

    Changed db data

--- store_rank_app/api/models.py
+++ store_rank_app/api/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Store(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     st_name = models.CharField(max_length=100)
     st_total_footfall = models.IntegerField()
     st_returning_footfall = models.IntegerField()
